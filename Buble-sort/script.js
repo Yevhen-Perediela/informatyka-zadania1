875d440dce001d88263c404caa4cd26fcbe19d50
Yevhen Perediela <93733182+Yevhen-Perediela@users.noreply.github.com>
2024-12-15 19:49:04 +0100

    Update script.js

--- Buble-sort/script.js
+++ Buble-sort/script.js
@@ -1,4 +1,4 @@
-function bubbleSort(arr) {
+function bubleSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
